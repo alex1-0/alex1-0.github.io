8172c2f241aa554e663cff3f790cbd0b7941721f
Alex <103360320+alex1-0@users.noreply.github.com>
2025-02-11 19:49:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('lateinButton').addEventListener('click', function() {
+    window.location.href = 'https://alex1-0.github.io/latein';
+});
+
+document.getElementById('parkfinderButton').addEventListener('click', function() {
+    window.location.href = 'https://alex1-0.github.io/parkfinder';
+});
