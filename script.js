2523869f54cba0ee024c15b69ce6c6791479174f
Alex <103360320+alex1-0@users.noreply.github.com>
2025-04-04 22:32:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('lateinButton').addEventListener('click', function() {
-    window.location.href = 'https://alex1-0.github.io/latein';
-});
-
-document.getElementById('parkfinderButton').addEventListener('click', function() {
-    window.location.href = 'https://alex1-0.github.io/parkfinder';
-});
